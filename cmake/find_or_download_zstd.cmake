2f60750da3dbd9747fc80c92b175907b9142f1e7
Davide Faconti <davide.faconti@gmail.com>
2026-03-01 19:36:55 +0100

    Rebase from 'release/kilted/cloudini_lib'

--- cmake/find_or_download_zstd.cmake
+++ cmake/find_or_download_zstd.cmake
@@ -1,7 +1,10 @@
 function(find_or_download_zstd FORCE_VENDORED)
 
   if(NOT FORCE_VENDORED)
-    find_package(ZSTD QUIET)
+    find_package(zstd CONFIG QUIET)
+    if(NOT TARGET zstd::libzstd_static)
+      find_package(ZSTD QUIET)
+    endif()
   endif()
 
   # Check if ZSTD targets already exist (e.g., from Arrow)
