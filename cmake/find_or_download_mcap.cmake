e8b3f948512d1978e9620a30724453e606941cc5
Davide Faconti <davide.faconti@gmail.com>
2026-03-04 16:07:03 +0100

    Rebase from 'release/humble/cloudini_lib'

--- cmake/find_or_download_mcap.cmake
+++ cmake/find_or_download_mcap.cmake
@@ -1,5 +1,18 @@
 function(find_or_download_mcap)
 
+  # Try system mcap_vendor first (installed on ROS buildfarm via package.xml build_depend)
+  if(NOT TARGET mcap AND NOT TARGET mcap_vendor::mcap)
+    find_package(mcap_vendor QUIET)
+  endif()
+
+  # mcap_vendor exports "mcap_vendor::mcap"; wrap it with compatible mcap/mcap::mcap aliases
+  if(TARGET mcap_vendor::mcap AND NOT TARGET mcap)
+    add_library(mcap INTERFACE)
+    target_link_libraries(mcap INTERFACE mcap_vendor::mcap)
+    add_library(mcap::mcap ALIAS mcap)
+    return()
+  endif()
+
   if(NOT TARGET mcap)
     message(STATUS "Downloading mcap")
     CPMAddPackage(
@@ -13,7 +26,6 @@ function(find_or_download_mcap)
     target_include_directories(mcap INTERFACE "${mcap_SOURCE_DIR}/cpp/mcap/include")
     ## create alias mcap::mcap
     add_library(mcap::mcap ALIAS mcap)
-
   endif()
 
 endfunction()
