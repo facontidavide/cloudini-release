97310b6389a9a88f5414dc34018d4416b09aa953
Davide Faconti <davide.faconti@gmail.com>
2026-04-06 12:51:25 +0200

    Revert "Trimmed the branch to only the cloudini_lib sub directory"
    This reverts commit 0fa2a46aec31fd267a215a52e502e30a3d696ef5.

---
+++ cloudini_foxglove/src/index.ts
@@ -0,0 +1,18 @@
+import { ExtensionContext, Immutable, MessageEvent } from "@foxglove/extension";
+import { convertPointCloudWasm, loadCloudiniWasm } from "./PointCloudConverter";
+
+import { CompressedPointCloud } from "./Schemas";
+
+export function activate(extensionContext: ExtensionContext): void {
+  // Preload WASM module
+  loadCloudiniWasm().catch(console.error);
+
+  extensionContext.registerMessageConverter<CompressedPointCloud>({
+    type: "schema",
+    fromSchemaName: "point_cloud_interfaces/msg/CompressedPointCloud2",
+    toSchemaName: "sensor_msgs/msg/PointCloud2",
+    converter: (inputMessage: CompressedPointCloud, _messageEvent: Immutable<MessageEvent<CompressedPointCloud>>) => {
+      return convertPointCloudWasm(inputMessage);
+    },
+  });
+}
